f3f9d1e3398dae452652d31dea4757eafd349b4e
priyangogoi <63924692+priyangogoi@users.noreply.github.com>
2022-02-13 18:56:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
           width: 100vw;
         }
     </style>
-    <script type="text/javascript" src="../GOOGLE_maps_API/javascript/script.js" defer></script>
+    <script type="text/javascript" src="/javascript/script.js" defer></script>
 </head>
 <body>
 
