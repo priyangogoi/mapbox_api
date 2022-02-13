766ec4e35a6cf42c93a227d76c4ce3d0d50ef316
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-13 17:07:16 +0530

    added index and js

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA';
+
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+const geolocate= new mapboxgl.GeolocateControl(
+  {
+    positionOptions: { enableHighAccuracy: true }, 
+    trackUserLocation: true,
+    showUserHeading: true
+  })
+map.addControl(geolocate);
+
+geolocate.on('geolocate', function(e) {
+      var lon = e.coords.longitude;
+      var lat = e.coords.latitude
+      var position = [lon, lat];
+      console.log(position);
+});
+
+   // When active the map will receive updates to the device's location as it changes.
+
+   // Draw an arrow next to the location dot to indicate which direction the device is heading.
