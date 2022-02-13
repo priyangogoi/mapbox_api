9afcae5a5be8cd5ecee75ce2a9a66f66c3a8cf71
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-13 20:36:22 +0530

    updated

--- javascript/script.js
+++ javascript/script.js
@@ -8,14 +8,15 @@ function successLocation(position){
   setupMap([position.coords.longitude, position.coords.latitude])
 }
 function errorLocation(){
- 
+  
 }
 
   function setupMap(center){
     var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11', 
-      center:center
+      center:center, 
+      zoom:15
     });
   }
 
