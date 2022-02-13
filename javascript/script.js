d2a261303d6fc5e7407334529ff140a1ef972ef1
priyangogoi <63924692+priyangogoi@users.noreply.github.com>
2022-02-13 19:17:54 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA';
 
 
-// var map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v11'
-// });
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+
 
 // navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
 //   enableHighAccuracy:true
