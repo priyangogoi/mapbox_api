0c0acc161e0932fbe7ea3e22d39db7e81f207ec6
priyangogoi <63924692+priyangogoi@users.noreply.github.com>
2022-02-13 19:09:21 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,35 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA';
 
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
-
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-  enableHighAccuracy:true
-})
-function successLocation(position){
-  console.log(position);
-}
-function errorLocation(){
-
-}
-
-// const geolocate= new mapboxgl.GeolocateControl(
-//   {
-//     positionOptions: { enableHighAccuracy: true }, 
-//     trackUserLocation: true,
-//     showUserHeading: true
-//   })
-// map.addControl(geolocate);
-
-// geolocate.on('geolocate', function(e) {
-//       var lon = e.coords.longitude;
-//       var lat = e.coords.latitude
-//       var position = [lon, lat];
-//       console.log(position);
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11'
 // });
+
+// navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+//   enableHighAccuracy:true
+// })
+// function successLocation(position){
+//   console.log(position);
+// }
+// function errorLocation(){
+
+// }
+
+const geolocate= new mapboxgl.GeolocateControl(
+  {
+    positionOptions: { enableHighAccuracy: true }, 
+    trackUserLocation: true,
+    showUserHeading: true
+  })
+map.addControl(geolocate);
+
+geolocate.on('geolocate', function(e) {
+      var lon = e.coords.longitude;
+      var lat = e.coords.latitude
+      var position = [lon, lat];
+      console.log(position);
+});
 
    // When active the map will receive updates to the device's location as it changes.
 
