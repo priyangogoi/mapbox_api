7675bc2fc9519dfa3519c547f4dbd81bd9aa2eab
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-13 19:37:00 +0530

    added php file

--- javascript/script.js
+++ javascript/script.js
@@ -6,21 +6,36 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v11'
 });
 
+// navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+//   enableHighAccuracy:true
+// })
+// function successLocation(position){
+//   console.log(position);
+// }
+// function errorLocation(){
+
+// }
+
+
+
 const geolocate= new mapboxgl.GeolocateControl(
   {
     positionOptions: { enableHighAccuracy: true }, 
+// When active the map will receive updates to the device's location as it changes.
     trackUserLocation: true,
+  // Draw an arrow next to the location dot to indicate which direction the device is heading.
     showUserHeading: true
   })
 map.addControl(geolocate);
 
 geolocate.on('geolocate', function(e) {
-      var lon = e.coords.longitude;
-      var lat = e.coords.latitude
-      var position = [lon, lat];
-      console.log(position);
+      // var lon = e.coords.longitude;
+      // var lat = e.coords.latitude;
+      // var position = [lon, lat];
+      //console.log(position);
+      console.log(e)
 });
 
-   // When active the map will receive updates to the device's location as it changes.
+ 
 
-   // Draw an arrow next to the location dot to indicate which direction the device is heading.
+ 
