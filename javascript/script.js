80167464adb69cfd7b7a1d95b64b52dc23d70f83
priyangogoi <63924692+priyangogoi@users.noreply.github.com>
2022-02-13 19:45:29 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -6,38 +6,6 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v11'
 });
 
-<<<<<<< HEAD
-// navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-//   enableHighAccuracy:true
-// })
-// function successLocation(position){
-//   console.log(position);
-// }
-// function errorLocation(){
-
-// }
-
-
-
-const geolocate= new mapboxgl.GeolocateControl(
-  {
-    positionOptions: { enableHighAccuracy: true }, 
-// When active the map will receive updates to the device's location as it changes.
-    trackUserLocation: true,
-  // Draw an arrow next to the location dot to indicate which direction the device is heading.
-    showUserHeading: true
-  })
-map.addControl(geolocate);
-
-geolocate.on('geolocate', function(e) {
-      // var lon = e.coords.longitude;
-      // var lat = e.coords.latitude;
-      // var position = [lon, lat];
-      //console.log(position);
-      console.log(e)
-});
-=======
-
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
   enableHighAccuracy:true
@@ -64,7 +32,7 @@ function errorLocation(){
 //       var position = [lon, lat];
 //       console.log(position);
 // });
->>>>>>> a924c887e04b20c177d210cd74d2510f9a2f52aa
+
 
  
 
