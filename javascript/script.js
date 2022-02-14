aaceee4473d330c84ef9b2ee72e0d078c1c4446c
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-14 15:34:38 +0530

    updated js

--- javascript/script.js
+++ javascript/script.js
@@ -1,80 +1,77 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA';
 
-// navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-//   enableHighAccuracy:true
-// })
-// function successLocation(position){
-//   console.log(position);
-//   setupMap([position.coords.longitude, position.coords.latitude])
-// }
-// function errorLocation(){
-//   setupMap([-2.24, 53.48])
-// }
-
-  // function setupMap(center){
-  //   var map = new mapboxgl.Map({
-  //     container: 'map',
-  //     style: 'mapbox://styles/mapbox/streets-v11', 
-  //     center:center, 
-  //     zoom:15
-  //   });
-     
-  //   const nav=new mapboxgl.NavigationControl();
-  //   map.addControl(nav);
-
-  //   var directions = new MapboxDirections({
-  //     accessToken: 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA',
-  //   });
-    
-  //   map.addControl(directions, 'top-left');    
-  // }
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11', 
+  center:[95.35680033297228, 27.488665589483862],
+  zoom:14
+});
 
 
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', 
-    zoom:5
+    // Add geolocate control to the map.
+  const geolocate=
+  new mapboxgl.GeolocateControl({
+  positionOptions: {
+  enableHighAccuracy: true
+  },
+  // When active the map will receive updates to the device's location as it changes.
+  trackUserLocation: true,
+  // Draw an arrow next to the location dot to indicate which direction the device is heading.
+  showUserHeading: true
   });
-   
-  
-const geolocate= new mapboxgl.GeolocateControl(
-  {
-    positionOptions: { enableHighAccuracy: true }, 
-   //When active the map will receive updates to the device's location as it changes.
-    trackUserLocation: true,
-  //Draw an arrow next to the location dot to indicate which direction the device is heading.
-
-    showUserHeading: true
-  })
-
+  //Adding geolocation control button
   map.addControl(geolocate);
+  //Adding Zoom control button 
+  const nav=new mapboxgl.NavigationControl()
+  map.addControl(nav);
 
-geolocate.on('geolocate', function(e) {
+  geolocate.on('geolocate', function(e) {
       var lon = e.coords.longitude;
       var lat = e.coords.latitude;
       var position = [lon, lat];
-      console.log(position);
-      setupMap(position);
+      if(position.length!=0){
+        setupMap(position);
+      }
+      else{
+        setupMap([95.37, 27.5])
+      }
 });
 
-const nav=new mapboxgl.NavigationControl();
-map.addControl(nav);
- 
-
 function setupMap(center){
-
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', 
     center:center, 
     zoom:15
   });
-   
-  var directions = new MapboxDirections({
-    accessToken: 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA',
-  });
-  
-  map.addControl(directions, 'top-left');    
+
+    var directions = new MapboxDirections({
+      accessToken: 'mapboxgl.accessToken',
+    });
+    
+    map.addControl(directions, 'top-left');    
+  }
 
   
-}
+// const geolocate= new mapboxgl.GeolocateControl(
+//   {
+//     positionOptions: { enableHighAccuracy: true }, 
+// When active the map will receive updates to the device's location as it changes.
+//     trackUserLocation: true,
+  // Draw an arrow next to the location dot to indicate which direction the device is heading.
+
+//     showUserHeading: true
+//   })
+// map.addControl(geolocate);
+
+// geolocate.on('geolocate', function(e) {
+//       // var lon = e.coords.longitude;
+//       // var lat = e.coords.latitude;
+//       // var position = [lon, lat];
+//       //console.log(position);
+//       console.log(e)
+// });
+
+ 
+
+ 
