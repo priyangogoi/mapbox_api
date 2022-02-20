5276234e7bde3045891c92d73a9c5867e08131a6
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-20 12:46:24 +0530

    asqwd

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ try{
 
     $myPDO=new PDO("pgsql:host=ec2-52-73-149-159.compute-1.amazonaws.com; dbname=d5amlc2e2jbdhf;", "mdywqgrhlhywqn", "bec6970d84f2b5b172ddb8164b05f1310f6cc185278913745fe6befd0e2efc40");
 
-    echo "Connected to POSTGRES';
+    echo '<script>alert("Connected to postgres");</script>';
 
 } catch(PDOException $e){
 
