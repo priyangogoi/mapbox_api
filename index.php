54bf179b76cfcc341691867662f139937fb4b99d
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-20 17:24:37 +0530

    up

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ try{
 
     $myPDO=new PDO("pgsql:host=ec2-52-73-149-159.compute-1.amazonaws.com; dbname=d5amlc2e2jbdhf;", "mdywqgrhlhywqn", "bec6970d84f2b5b172ddb8164b05f1310f6cc185278913745fe6befd0e2efc40");
 
-    echo "Connected to postgres";
+    $sql= "SELECT * FROM users";
+    foreach($myPDO->query($sql) as $row){
+      echo "<br/>";
+      print $row['userid'].'-'.$row['userphone'].'-'.$row['userpwd'];
+    }
 
 } catch(PDOException $e){
 
