a618999ec52ace7e7d0fd90a3d613bc86d4ea560
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-20 18:21:46 +0530

    insert command

--- index.php
+++ index.php
@@ -15,10 +15,13 @@ try{
 
     $myPDO=new PDO("pgsql:host=ec2-52-73-149-159.compute-1.amazonaws.com; dbname=d5amlc2e2jbdhf;", "mdywqgrhlhywqn", "bec6970d84f2b5b172ddb8164b05f1310f6cc185278913745fe6befd0e2efc40");
 
-    $sql= "SELECT * FROM users";
-    foreach($myPDO->query($sql) as $row){
-      echo "<br/>";
-      print $row['userid'].'-'.$row['userphone'].'-'.$row['userpwd'];
+    $sqlins= "INSERT INTO users (userphone, userpwd) VALUES (876567432, '12adwdred')";
+    $myPDO->query($sql);
+
+    $sqlfetc="SELECT * FROM users";
+    foreach($myPDO->query($sqlfetc) as $row){
+      print "<br/>";
+      print $row['userid'].'-'.$row['userphone'].'-'.$row['userpwd'].'<br/>';
     }
 
 } catch(PDOException $e){
