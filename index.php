7a57b6cc832cdce77f1b5a573213c119446c31db
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-21 16:07:00 +0530

    adding serial

--- index.php
+++ index.php
@@ -9,27 +9,32 @@
 </head>
 <body>
 
+<div class="container">
+  <div class="section">
+    <form id="signupform" method="post">
+      
+    </form>
+  </div>
+</div>
 <?php
 
 try{
 
-    $myPDO=new PDO("pgsql:host=ec2-52-73-149-159.compute-1.amazonaws.com; dbname=d5amlc2e2jbdhf;", "mdywqgrhlhywqn", "bec6970d84f2b5b172ddb8164b05f1310f6cc185278913745fe6befd0e2efc40");
+  $myPDO=new PDO("pgsql:host=ec2-52-73-149-159.compute-1.amazonaws.com; dbname=d5amlc2e2jbdhf;", "mdywqgrhlhywqn", "bec6970d84f2b5b172ddb8164b05f1310f6cc185278913745fe6befd0e2efc40");
 
-    $sql= "DELETE from users where userid=2";
-    $myPDO->query($sql);
-    $sqlfetc="SELECT * FROM users";
-    foreach($myPDO->query($sqlfetc) as $row){
-      print "<br/>";
-      print $row['userid'].'-'.$row['userphone'].'-'.$row['userpwd'].'<br/>';
-    }
+  $sqlins= "INSERT INTO users(userid, userphone, userpwd) VALUES ('2', '876567432', '12adwdred')";
+  $myPDO->query($sqlins);
+  $sqlfetc="SELECT * FROM users";
+  foreach($myPDO->query($sqlfetc) as $row){
+    print "<br/>";
+    print $row['userid'].'-'.$row['userphone'].'-'.$row['userpwd'].'<br/>';
+  }
 
 } catch(PDOException $e){
 
-    echo $e->getMessage();
+  echo $e->getMessage();
 }
-
 ?>
-
 </body>
 </html>   
     <!-- <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
