3bf01e2974f3955a61d23c8baaa0861a9684a661
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-22 19:49:57 +0530

    gffwf

--- frontend/frontScript.js
+++ frontend/frontScript.js
@@ -1,26 +1,3 @@
-const phone=document.getElementById("signupPhone");
-  const pwd=document.getElementById("signupPwd");
-document.getElementById("signupform").addEventListener("submit", (e)=>{
-  e.preventDefault();
-  if(phone.value==="" || pwd.value===""){
-   alert("Enter all the fields")
-  }
-  else{
-    let stmt="phone="+phone.value+"&pwd="+pwd.value;
-    let xhr=new XMLHttpRequest();
-    xhr.open("POST", "conn.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onload=function(){
-      console.log(this.responseText);
-    }
-    xhr.send(stmt);
-  }
- 
-  
-})
-  phone.value="";
-  pwd.value="";
-
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJpeWFuZ29nb2kiLCJhIjoiY2t6am95bm42MG5nNzJ1bGw3cnY0b3g0cyJ9.hzWoULXbbjFmxwbmmstHWA';
 
