b4d6e5d5166c7652b5d8bc68580da875c195f3b4
priyangogoi <priyanujgogoi180@gmail.com>
2022-02-23 10:49:24 +0530

    wefwefwe

--- javascript/script.js
+++ javascript/script.js
@@ -11,7 +11,7 @@ document.getElementById("signupform").addEventListener("submit", (e)=>{
     xhr.open("POST", "conn.php", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.onload=function(){
-    document.querySelector(".main").innerHTML+=this.responseText;
+    location.href='./frontend/home.php';
     }
     xhr.send(stmt);
   }
